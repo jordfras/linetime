038b53739385d4ae6dc1ab69e9f968e785554632
Thomas Johannesson <jordfras@gmail.com>
2024-10-23 21:15:23 +0200

    Add embryo of reading tokens and print time of each line

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,42 @@
 use std::io::Read;
+use std::time::{Duration, SystemTime};
+
+#[derive(PartialEq)]
+enum CursorMove {
+    Home,
+    ToLineAndColumn((u32, u32)),
+    LinesUp(u32),
+    LinesDown(u32),
+    ColumnsRight(u32),
+    ColumnsLeft(u32),
+    UpOne,
+    SavePosition,
+    RestorePosition,
+}
+
+#[derive(PartialEq)]
+enum Token {
+    // A single character
+    Char(char),
+    CarriageReturn,
+    LineFeed,
+    // An escape sequence (starting with ESC[ ) to control terminal behavior
+    EscapeMoveSequence(CursorMove),
+    // End of file, i.e., end of input stream
+    EndOfFile,
+}
+
+impl std::fmt::Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Token::Char(c) => write!(f, "{c}"),
+            Token::CarriageReturn => write!(f, "<CR>"),
+            Token::LineFeed => write!(f, "<LF>\n"),
+            Token::EscapeMoveSequence(_) => write!(f, "<MOVE>"),
+            Token::EndOfFile => write!(f, "EOF"),
+        }
+    }
+}
 
 /// Reads a character, if EOF is reached None is returned
 fn read_char(stream: &mut impl Read) -> Result<Option<char>, std::io::Error> {
@@ -28,18 +66,55 @@ fn read_char(stream: &mut impl Read) -> Result<Option<char>, std::io::Error> {
     Err(std::io::Error::other("Invalid UTF-8 character"))
 }
 
+fn read_token(stream: &mut impl Read) -> Result<Token, std::io::Error> {
+    if let Some(c) = read_char(stream)? {
+        Ok(match c {
+            '\r' => Token::CarriageReturn,
+            '\n' => Token::LineFeed,
+            // TODO: Escape sequencees
+            _ => Token::Char(c),
+        })
+    } else {
+        Ok(Token::EndOfFile)
+    }
+}
+
+fn format(duration: Duration) -> String {
+    format!(
+        "{:0>2}:{:0>2}.{:0>3}",
+        duration.as_secs() / 60,
+        duration.as_secs() % 60,
+        duration.subsec_millis()
+    )
+}
+
 fn main() {
+    let start_time = SystemTime::now();
     let mut stdin = std::io::stdin().lock();
-    while let Ok(c) = read_char(&mut stdin) {
-        if let Some(c) = c {
-            println!("char: {c}");
-        } else {
-            println!("EOF");
-            return;
+
+    let mut start_of_line = true;
+    loop {
+        match read_token(&mut stdin) {
+            Ok(Token::EndOfFile) => {
+                println!("{}", Token::EndOfFile);
+                break;
+            }
+            Ok(token) => {
+                if start_of_line {
+                    print!(
+                        "{}: ",
+                        format(SystemTime::now().duration_since(start_time).unwrap())
+                    );
+                }
+                print!("{}", token);
+                start_of_line = token == Token::LineFeed;
+            }
+            Err(error) => {
+                eprintln!("Error reading from stdin: {error}");
+                std::process::exit(1);
+            }
         }
     }
-
-    println!("ERROR");
 }
 
 #[cfg(test)]
